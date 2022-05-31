68863845583aae92832445cf82c76445369f49f4
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-05-31 17:44:22 +0300

    add hw

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const volleyball = require('volleyball');
 const compression = require('compression');
 const path = require('path');
-// Імпортувати cookie-parser
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.set('views', path.join(process.cwd(), 'views'));
 app.use(express.urlencoded({ extended: false }));
 app.use(volleyball);
 app.use(compression());
-// Підключити cookiesParser
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -25,12 +25,13 @@ app.get('/login', (req, res) => {
 
 app.get('/profile', (req, res) => {
   res.render('profile', {
-    username: '', // username потрібно взяти з куків
+    username: req.cookies.username, 
   });
 });
 
 app.post('/login', (req, res) => {
-  // Зберегти в куки username з req.body.username
+  console.log(req);
+  res.cookie('username', req.body.username);
   res.redirect('/profile');
 });
 
